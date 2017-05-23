50fc5ae27c49d06ff6e4852d345c019f5a8248d9
Aman Pratap Singh <aps10@iitbbs.ac.in>
2017-05-23 18:28:03 +0530

    Update #2

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,15 @@
 var attribute = ["Aman Pratap Singh",
                  "an Indian",
-                 "a Computer Science Undergraduate",
+                 "a CS undergraduate ",
                  "an IITian",
                  "a CS:Go Fanatic",
                  "a Quoraphile",
                  "an ambivert",
                  "an agnostic",
                  "a programmer",
+                 "eating pizza",
                  "interested in MEAN stack",
-                 "looking for some cool projects"];
+                 "solving Codechef problem"];
 
 var len = attribute.length;
 
@@ -47,11 +48,17 @@ $(document).ready(function(){
     $(".attribute").css("opacity", 1);
   }, 1500);
 
-  if($(window).width() >= 1024){
-    $(".main-btn").click();
-  };
+
   $(".main-btn").click(function(){
     $(".main-btn").toggleClass("pulse");
   });
 
+  if($(window).width() >= 1024){
+    $(".main-btn").toggleClass("pulse");
+    $('.social-icons').append('<li><a class="btn-floating" style="background-color: #0077B5;" href="https://www.linkedin.com/in/aman-pratap-singh-06bab5126/" target="_blank"><i class="fa fa-linkedin"></i></a></li>');
+    $('.social-icons').append('<li><a class="btn-floating" style="background-color: #02b875;" href="https://medium.com/@amanpratapsingh" target="_blank"><i class="fa fa-medium"></i></a></li>');
+    $('.social-icons').append('<li><a class="btn-floating pink darken-2"><i class="fa fa-instagram" href="https://www.instagram.com/apsknight" target="_blank"></i></a></li>');
+    $('.social-icons').append('<li><a class="btn-floating indigo darken-4"><i class="fa fa-steam" href="http://steamcommunity.com/id/amanpratapsingh/" target="_blank"></i></a></li>');
+    $(".main-btn").click();
+  };
 });
